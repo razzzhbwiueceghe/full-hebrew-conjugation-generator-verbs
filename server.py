0bc2276153d4a9793b1607cd23063e8a503a0343
Jack Harris <jackharris@Jacks-MacBook-Pro.local>
2024-05-01 18:17:44 -0400

    configure for heroku

--- server.py
+++ server.py
@@ -1823,7 +1823,8 @@ if __name__ == '__main__':
 
 	# run() method of Flask class runs the application
 	# on the local development server.
-	app.run(port=33507)
+	port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)
 
 
 
